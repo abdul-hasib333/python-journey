bdd68dc62ab9abee58ebd726869cc755eea4b227
Abdul Hasib <hasibofficial333@gmail.com>
2026-04-21 23:11:54 +0100

    Fix spacing in print and typo in comment

--- day2.py
+++ day2.py
@@ -1,4 +1,4 @@
-#Day 2: Varisbles, inputs and Basic Math
+#Day 2: Variables, inputs and Basic Math
 
 name = input("What is your name?  ")
 age = input("How Old are you?")
@@ -7,4 +7,4 @@ age_number = int(age)
 years_until_30 = 30 - age_number
 
 print("Hello " + name + "!")
-print("You will be 30 in" + str(years_until_30) + "years.")
+print("You will be 30 in " + str(years_until_30) + " years. ")
